efdacfd85c1afc0a9207b91aad18a49f4996587a
Andrea Medeghini <andrea@nextbreakpoint.com>
2019-02-09 17:30:31 +0000

    wip

--- src/main/kotlin/com/nextbreakpoint/Hello.kt
+++ src/main/kotlin/com/nextbreakpoint/Hello.kt
@@ -2,19 +2,37 @@ package com.nextbreakpoint
 
 import io.kubernetes.client.apis.CoreV1Api
 import io.kubernetes.client.Configuration
-import io.kubernetes.client.models.V1DeleteOptions
+import io.kubernetes.client.models.*
 import io.kubernetes.client.util.Config
+import java.io.File
+import java.io.FileInputStream
 
 fun main(args: Array<String>) {
-    val client = Config.defaultClient()
+    val client = Config.fromConfig(FileInputStream(File("/Users/andrea/Desktop/kubernetes-playground/admin.conf")))
 
     Configuration.setDefaultApiClient(client)
 
     val api = CoreV1Api()
 
-    printAllPods(api)
+    var pod = createPod(api)
 
-    deletePod(api, args[0])
+//    printAllPods(api)
+
+    deletePod(api, pod.metadata.name)
+}
+
+fun createPod(api: CoreV1Api) : V1Pod {
+    val container = V1Container()
+    container.image = "busybox"
+    container.name = "mybox"
+    container.command = listOf("ls", "/")
+//    container.tty(true)
+    val v1Pod = V1Pod()
+        .metadata(V1ObjectMeta().generateName("busybox-"))
+        .spec(V1PodSpec().containers(listOf(container)).restartPolicy("Never"))
+    var pod = api.createNamespacedPod("default", v1Pod, null, null, null)
+    println("Pod startus ${pod.metadata.name}")
+    return pod;
 }
 
 private fun printAllPods(api: CoreV1Api) {
@@ -23,11 +41,16 @@ private fun printAllPods(api: CoreV1Api) {
     list.items.forEach {
         item -> println("Pod name: ${item.metadata.name}")
     }
+
+    list.items.forEach {
+            item -> println("${item.status}")
+    }
 }
 
 private fun deletePod(api: CoreV1Api, name: String) {
-    val status = api.deleteNamespacedPod(name, "default", V1DeleteOptions(), null, null, null, null)
+    val status = api.deleteNamespacedPod(name, "default", V1DeleteOptions(), "true", null, null, null, null)
 
-    println("Response status: ${status.status}")
+    println("Response status: ${status.reason}")
+    status.details.causes.forEach { println(it.message) }
 }
 
