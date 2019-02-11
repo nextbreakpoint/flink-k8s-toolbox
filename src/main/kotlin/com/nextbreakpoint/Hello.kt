1b5a9530cb83988d7b6fd4c85aeb7bbbc9cdf236
Andrea Medeghini <andrea@nextbreakpoint.com>
2019-02-11 07:26:43 +0000

    wip

--- src/main/kotlin/com/nextbreakpoint/Hello.kt
+++ src/main/kotlin/com/nextbreakpoint/Hello.kt
@@ -1,5 +1,6 @@
 package com.nextbreakpoint
 
+import com.google.gson.JsonParseException
 import io.kubernetes.client.apis.CoreV1Api
 import io.kubernetes.client.Configuration
 import io.kubernetes.client.models.*
@@ -8,31 +9,58 @@ import java.io.File
 import java.io.FileInputStream
 
 fun main(args: Array<String>) {
-    val client = Config.fromConfig(FileInputStream(File("/Users/andrea/Desktop/kubernetes-playground/admin.conf")))
+    val api = coreV1Api()
+
+    if (args[0].equals("create")) {
+        createFlinkCluster(api)
+    }
+
+    if (args[0].equals("list")) {
+        printAllPods(api)
+    }
+
+//    deletePod(api, pod.metadata.name)
+}
+
+private val configPath = "/Users/andrea/Documents/projects/infrastructure/kubernetes-playground/admin.conf"
+
+private fun coreV1Api(): CoreV1Api {
+    val client = Config.fromConfig(FileInputStream(File(configPath)))
 
     Configuration.setDefaultApiClient(client)
 
     val api = CoreV1Api()
 
-    var pod = createPod(api)
-
-//    printAllPods(api)
-
-    deletePod(api, pod.metadata.name)
+    return api
 }
 
-fun createPod(api: CoreV1Api) : V1Pod {
-    val container = V1Container()
-    container.image = "busybox"
-    container.name = "mybox"
-    container.command = listOf("ls", "/")
-//    container.tty(true)
-    val v1Pod = V1Pod()
-        .metadata(V1ObjectMeta().generateName("busybox-"))
-        .spec(V1PodSpec().containers(listOf(container)).restartPolicy("Never"))
-    var pod = api.createNamespacedPod("default", v1Pod, null, null, null)
-    println("Pod startus ${pod.metadata.name}")
-    return pod;
+fun createFlinkCluster(api: CoreV1Api) {
+    val jobmanager = V1Container()
+    jobmanager.image = "flink:1.7.1-scala_2.11-alpine"
+    jobmanager.name = "flink-jobmanager"
+    jobmanager.args = listOf("jobmanager")
+    jobmanager.ports = listOf(V1ContainerPort().containerPort(8081).hostPort(8081), V1ContainerPort().containerPort(6123))
+
+    val taskmanager = V1Container()
+    taskmanager.image = "flink:1.7.1-scala_2.11-alpine"
+    taskmanager.name = "flink-taskmanager"
+    taskmanager.args = listOf("taskmanager")
+    taskmanager.ports = listOf(V1ContainerPort().containerPort(6121), V1ContainerPort().containerPort(6122))
+    taskmanager.env = listOf(V1EnvVar().name("JOB_MANAGER_RPC_ADDRESS").value("flink-jobmanager.default.pod.cluster.local"))
+
+    val jobmanagerPodIn = V1Pod()
+        .metadata(V1ObjectMeta().generateName("flink-jobmanager-"))
+        .spec(V1PodSpec().hostname("flink-jobmanager").containers(listOf(jobmanager)).restartPolicy("Never"))
+
+    val taskmanagerPodIn = V1Pod()
+        .metadata(V1ObjectMeta().generateName("flink-taskmanager-"))
+        .spec(V1PodSpec().containers(listOf(taskmanager)).restartPolicy("Never"))
+
+    val jobmanagerPodOut = api.createNamespacedPod("default", jobmanagerPodIn, null, null, null)
+    println("Job Manager created ${jobmanagerPodOut.metadata.name}")
+
+    val taskmanagerPodOut = api.createNamespacedPod("default", taskmanagerPodIn, null, null, null)
+    println("Task Manager created ${taskmanagerPodOut.metadata.name}")
 }
 
 private fun printAllPods(api: CoreV1Api) {
@@ -48,9 +76,14 @@ private fun printAllPods(api: CoreV1Api) {
 }
 
 private fun deletePod(api: CoreV1Api, name: String) {
-    val status = api.deleteNamespacedPod(name, "default", V1DeleteOptions(), "true", null, null, null, null)
+    try {
+        val status = api.deleteNamespacedPod(name, "default", V1DeleteOptions(), "true", null, null, null, null)
 
-    println("Response status: ${status.reason}")
-    status.details.causes.forEach { println(it.message) }
+        println("Response status: ${status.reason}")
+
+        status.details.causes.forEach { println(it.message) }
+    } catch (e : JsonParseException) {
+        // ignore. see bug https://github.com/kubernetes/kubernetes/issues/59501
+    }
 }
 
