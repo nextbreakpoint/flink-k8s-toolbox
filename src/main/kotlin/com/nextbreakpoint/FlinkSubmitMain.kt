604bc04ea7b7cb23296b0279c0f10d6fbe3e27dc
Andrea Medeghini <andrea@nextbreakpoint.com>
2019-03-23 13:05:38 +0000

    refactor

---
+++ src/main/kotlin/com/nextbreakpoint/FlinkSubmitMain.kt
@@ -0,0 +1,10 @@
+package com.nextbreakpoint
+
+class FlinkSubmitMain {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            FlinkSubmit().main(args)
+        }
+    }
+}
