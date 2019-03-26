44abe61fd131f352a55028aa16a12e34e6c99397
Andrea Medeghini <andrea@nextbreakpoint.com>
2019-03-26 08:51:08 +0000

    review parameters

--- src/main/kotlin/com/nextbreakpoint/FlinkSubmitMain.kt
+++ src/main/kotlin/com/nextbreakpoint/FlinkSubmitMain.kt
@@ -2,10 +2,7 @@ package com.nextbreakpoint
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
-import com.github.ajalt.clikt.parameters.options.default
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.prompt
-import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.options.*
 import com.github.ajalt.clikt.parameters.types.float
 import com.github.ajalt.clikt.parameters.types.int
 import com.nextbreakpoint.command.*
@@ -25,25 +22,26 @@ class FlinkSubmitMain {
         }
     }
 
-    class FlinkSubmit: CliktCommand() {
+    class FlinkSubmit: CliktCommand(name = "FlinkSubmit") {
         override fun run() = Unit
     }
 
     class Create: CliktCommand(help="Create a cluster") {
         private val kubeConfig: String by option(help="The path of Kubectl config").required()
-        private val dockerImage: String by option(help="The name of Docker image with Flink job").required()
-        private val clusterName: String by option(help="The name of the Flink cluster").required()
+        private val clusterName: String by option(help="The name of the new Flink cluster").required()
         private val namespace: String by option(help="The namespace where to create the resources").default("default")
         private val environment: String by option(help="The name of the environment").default("test")
-        private val storageClass: String by option(help="The name of the storage class").default("standard")
-        private val pullPolicy: String by option(help="The image pull policy").default("IfNotPresent")
-        private val pullSecrets: String by option(help="The image pull secrets").required()
-        private val jobmanagerLimitCpus: Float by option(help="The JobManager's cpus limit").float().default(1f)
-        private val taskmanagerLimitCpus: Float by option(help="The TaskManager's cpus limit").float().default(1f)
-        private val jobmanagerLimitMemory: Int by option(help="The JobManager's memory limit in Mb").int().default(512)
-        private val taskmanagerLimitMemory: Int by option(help="The TaskManager's memory limit in Mb").int().default(1024)
-        private val jobmanagerStorage: Int by option(help="The JobManager's storage size in Gb").int().default(2)
-        private val taskmanagerStorage: Int by option(help="The TaskManager's storage size in Gb").int().default(5)
+        private val image: String by option(help="The Flink image to use for JobManager and TaskManager").required()
+        private val imagePullPolicy: String by option(help="The image pull policy").default("IfNotPresent")
+        private val imagePullSecrets: String by option(help="The image pull secrets").required()
+        private val jobmanagerCpus: Float by option(help="The JobManager's cpus limit").float().default(1f)
+        private val taskmanagerCpus: Float by option(help="The TaskManager's cpus limit").float().default(1f)
+        private val jobmanagerMemory: Int by option(help="The JobManager's memory limit in Mb").int().default(512)
+        private val taskmanageMemory: Int by option(help="The TaskManager's memory limit in Mb").int().default(1024)
+        private val jobmanagerStorageSize: Int by option(help="The JobManager's storage size in Gb").int().default(2)
+        private val taskmanagerStorageSize: Int by option(help="The TaskManager's storage size in Gb").int().default(5)
+        private val jobmanagerStorageClass: String by option(help="The JobManager's storage class").default("standard")
+        private val taskmanagerStorageClass: String by option(help="The TaskManager's storage class").default("standard")
         private val taskmanagerTaskSlots: Int by option(help="The number of task slots for each TaskManager").int().default(1)
         private val taskmanagerReplicas: Int by option(help="The number of TaskManager replicas").int().default(1)
 
@@ -55,31 +53,31 @@ class FlinkSubmitMain {
                     environment = environment
                 ),
                 jobmanager = JobManagerConfig(
-                    image = dockerImage,
-                    pullPolicy = pullPolicy,
-                    pullSecrets = pullSecrets,
+                    image = image,
+                    pullPolicy = imagePullPolicy,
+                    pullSecrets = imagePullSecrets,
                     storage = StorageConfig(
-                        size = jobmanagerStorage,
-                        storageClass = storageClass
+                        size = jobmanagerStorageSize,
+                        storageClass = jobmanagerStorageClass
                     ),
                     resources = ResourcesConfig(
-                        cpus = jobmanagerLimitCpus,
-                        memory = jobmanagerLimitMemory
+                        cpus = jobmanagerCpus,
+                        memory = jobmanagerMemory
                     )
                 ),
                 taskmanager = TaskManagerConfig(
-                    image = dockerImage,
-                    pullPolicy = pullPolicy,
-                    pullSecrets = pullSecrets,
+                    image = image,
+                    pullPolicy = imagePullPolicy,
+                    pullSecrets = imagePullSecrets,
                     taskSlots = taskmanagerTaskSlots,
                     replicas = taskmanagerReplicas,
                     storage = StorageConfig(
-                        size = taskmanagerStorage,
-                        storageClass = storageClass
+                        size = taskmanagerStorageSize,
+                        storageClass = taskmanagerStorageClass
                     ),
                     resources = ResourcesConfig(
-                        cpus = taskmanagerLimitCpus,
-                        memory = taskmanagerLimitMemory
+                        cpus = taskmanagerCpus,
+                        memory = taskmanageMemory
                     )
                 )
             )
@@ -110,8 +108,8 @@ class FlinkSubmitMain {
         private val environment: String by option(help="The name of the environment").default("test")
         private val className: String by option(help="The name of the class to submit").required()
         private val jarPath: String by option(help="The path of the jar to submit").required()
-        private val savepoint: String by option(help="The path of the jar to submit").default("")
-        private val arguments: String by option(help="The arguments to submit").default("")
+        private val arguments: String by option(help="The comma-separated list of arguments").default("")
+        private val savepoint: String by option(help="The path of the savepoint directory").default("")
 
         override fun run() {
             val config = JobSubmitConfig(
@@ -159,14 +157,18 @@ class FlinkSubmitMain {
         private val namespace: String by option(help="The namespace where to create the resources").default("default")
         private val clusterName: String by option(help="The name of the Flink cluster").required()
         private val environment: String by option(help="The name of the environment").default("test")
+        private val running: Boolean by option(help="List only running jobs").flag("true")
 
         override fun run() {
-            val descriptor = ClusterDescriptor(
-                namespace = namespace,
-                name = clusterName,
-                environment = environment
+            val config = JobListConfig(
+                descriptor = ClusterDescriptor(
+                    namespace = namespace,
+                    name = clusterName,
+                    environment = environment
+                ),
+                running = running
             )
-            ListJobs().run(kubeConfig, descriptor)
+            ListJobs().run(kubeConfig, config)
         }
     }
 }
