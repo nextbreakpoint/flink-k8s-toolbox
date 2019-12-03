71fa47278971cdc09d6e1d1c5a5b54c01ec111ae
Andrea Medeghini <andrea@nextbreakpoint.com>
2019-12-03 22:20:46 +0000

    provision integration machine using non root user

--- pipeline/setup-kube.sh
+++ pipeline/setup-kube.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-#echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo add-apt-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 sudo apt update -y && sudo apt -y install kubectl
