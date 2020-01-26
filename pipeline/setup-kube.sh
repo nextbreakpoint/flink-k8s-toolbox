365bbd2e826593b9bd27faf4eb51a2a2c07dbad5
Andrea Medeghini <andrea@nextbreakpoint.com>
2020-01-26 15:05:26 +0000

    improve scripts

--- pipeline/setup-kube.sh
+++ pipeline/setup-kube.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+set -x
+set -e
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo add-apt-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 sudo apt update -y && sudo apt -y install kubectl
