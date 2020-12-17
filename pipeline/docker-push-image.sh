04ef61679d42a60f438845f327f5cca97825d9ae
Andrea Medeghini <andrea@nextbreakpoint.com>
2020-12-17 22:53:46 +0000

    push image to dockerhub

--- pipeline/docker-push-image.sh
+++ pipeline/docker-push-image.sh
@@ -6,5 +6,5 @@ set -e
 eval $(minikube docker-env)
 
 docker tag integration/flinkctl:1.4.0-beta nextbreakpoint/flinkctl:1.4.0-beta
-docker login --username=$DOCKER_PASSWORD --email=$DOCKER_USERNAME
+docker login --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD
 docker push nextbreakpoint/flinkctl:1.4.0-beta
