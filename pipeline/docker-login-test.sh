fda3fe7a85e840684efddd26c9a5e00590104409
Andrea Medeghini <andrea@nextbreakpoint.com>
2020-12-18 17:22:48 +0000

    secure login

--- pipeline/docker-login-test.sh
+++ pipeline/docker-login-test.sh
@@ -5,6 +5,4 @@ set -e
 
 eval $(minikube docker-env)
 
-docker tag integration/flinkctl:1.4.0-beta nextbreakpoint/flinkctl:$DOCKER_IMAGE_TAG
 echo -n $DOCKER_PASSWORD | docker login --username=$DOCKER_USERNAME --password-stdin
-docker push nextbreakpoint/flinkctl:$DOCKER_IMAGE_TAG
