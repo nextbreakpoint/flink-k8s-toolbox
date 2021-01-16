b8fce2b62fe2a858f484a999d100f37982c85f21
Andrea Medeghini <andrea@nextbreakpoint.com>
2021-01-16 15:14:17 +0000

    update example and tests to flink 1.12

--- pipeline/run-integration-tests.sh
+++ pipeline/run-integration-tests.sh
@@ -5,5 +5,12 @@ set -e
 
 export OPERATOR_HOST=$(minikube ip)
 
+docker tag -t integration/flink:latest integration/flink:1.12.1
+export FLINK_VERSION=1.12.1
+export SCALA_VERSION=2.12
 ./gradlew integrationTest --info
 
+docker tag -t integration/flink:latest integration/flink:1.11.3
+export FLINK_VERSION=1.11.3
+export SCALA_VERSION=2.12
+./gradlew integrationTest --info
