d61f31567ea0585736c2bc38877800f36c56ec95
Andrea Medeghini <andrea@nextbreakpoint.com>
2021-01-17 10:34:31 +0000

    update kubernetes version in pipeline

--- pipeline/run-minikube.sh
+++ pipeline/run-minikube.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-minikube start --cpus=2 --memory=6gb --vm-driver=docker --kubernetes-version v1.18.0
+minikube start --cpus=2 --memory=6gb --vm-driver=docker --kubernetes-version v1.18.14
 #chown -R $USER $HOME/.kube $HOME/.minikube
 #chgrp -R $USER $HOME/.kube $HOME/.minikube
 
